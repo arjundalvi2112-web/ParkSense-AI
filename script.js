2d350526acc7abe3338ef48f6878dfac548306f5
arjundalvi2112-web <arjundalvi2112@gmail.com>
2026-07-17 20:21:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,195 +1,50 @@
-/* =========================
-   AI PARKING FINDER
-   JAVASCRIPT
-   PART 3
-========================= */
+
+// =======================
+// BOOKING SYSTEM
+// =======================
 
 
-// Smooth scrolling for navigation
-
-document.querySelectorAll("nav a").forEach(link => {
-
-    link.addEventListener("click", function(e){
-
-        e.preventDefault();
-
-        let target =
-        document.querySelector(this.getAttribute("href"));
-
-        target.scrollIntoView({
-
-            behavior:"smooth"
-
-        });
-
-    });
-
-});
+function bookParking(place,slot){
 
 
+document.getElementById("bookingBox")
+.style.display="flex";
 
 
+document.getElementById("bookingDetails")
+.innerHTML=
 
-// Button Animation
+`
+Parking Location:
+<b>${place}</b>
+<br><br>
 
-const button =
-document.querySelector(".hero-text button");
+Your Slot:
+<b>${slot}</b>
 
+<br><br>
 
-button.addEventListener("click",()=>{
+Time:
+<b>2 Hours</b>
 
+<br><br>
 
-    alert(
-    "🚗 Searching for the nearest available parking space..."
-    );
+Amount:
+<b>₹80</b>
 
+<br><br>
 
-});
-
-
-
-
-
-// Feature cards reveal animation
-
-
-const cards =
-document.querySelectorAll(".feature-card");
-
-
-const observer =
-new IntersectionObserver(entries=>{
-
-
-entries.forEach(entry=>{
-
-
-if(entry.isIntersecting){
-
-
-entry.target.style.opacity="1";
-
-entry.target.style.transform="translateY(0)";
-
+✅ Booking Successful!
+`;
 
 }
 
 
-});
 
 
-});
+function closeBooking(){
 
-
-
-cards.forEach(card=>{
-
-
-card.style.opacity="0";
-
-card.style.transform=
-"translateY(50px)";
-
-
-card.style.transition=
-"0.8s";
-
-
-observer.observe(card);
-
-
-});
-
-
-
-
-
-// AI Loading Simulation
-
-
-function startAI(){
-
-console.log(
-"AI Parking System Activated..."
-);
-
-
-let messages=[
-
-"Scanning nearby parking areas...",
-
-"Analyzing traffic conditions...",
-
-"Predicting parking availability...",
-
-"Best parking location found!"
-
-];
-
-
-let i=0;
-
-
-let interval=setInterval(()=>{
-
-
-console.log(messages[i]);
-
-
-i++;
-
-
-if(i>=messages.length){
-
-clearInterval(interval);
+document.getElementById("bookingBox")
+.style.display="none";
 
 }
-
-
-},1000);
-
-
-}
-
-
-startAI();
-
-
-
-
-
-// Dynamic year in footer (for future)
-
-let year =
-new Date().getFullYear();
-
-
-console.log(
-"AI Parking Finder © "+year
-);
-
-
-
-
-
-// Mouse movement glow effect
-
-
-document.addEventListener(
-"mousemove",
-(e)=>{
-
-
-let x=e.clientX;
-
-let y=e.clientY;
-
-
-document.body.style.background=
-
-`radial-gradient(circle at ${x}px ${y}px,
-rgba(0,102,255,0.08),
-#f4f8ff 300px)`;
-
-
-});
