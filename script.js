c3243578f21497b1727f6a1cc72b23d2294dee0f
arjundalvi2112-web <arjundalvi2112@gmail.com>
2026-07-17 20:25:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,6 +43,68 @@ Amount:
 
 
 function closeBooking(){
+  // SEARCH DEMO
+
+function searchParking(){
+
+let place =
+document.getElementById("searchBox").value;
+
+let result =
+document.getElementById("searchResult");
+
+if(place===""){
+
+result.innerHTML="Enter a location.";
+
+return;
+
+}
+
+result.innerHTML=
+
+"✅ Best parking found near <b>"+place+
+"</b><br><br>"+
+"Available Slots: 18<br>"+
+"AI Confidence: 94%<br>"+
+"Walking Distance: 120m";
+
+}
+
+
+
+// DARK MODE
+
+const darkBtn =
+document.getElementById("darkModeBtn");
+
+let dark=false;
+
+darkBtn.onclick=function(){
+
+dark=!dark;
+
+document.body.style.transition=".5s";
+
+if(dark){
+
+document.body.style.background="#111";
+
+document.body.style.color="white";
+
+darkBtn.innerHTML="☀️";
+
+}else{
+
+document.body.style.background="#f4f8ff";
+
+document.body.style.color="#222";
+
+darkBtn.innerHTML="🌙";
+
+}
+
+}
 
 document.getElementById("bookingBox")
 .style.display="none";
